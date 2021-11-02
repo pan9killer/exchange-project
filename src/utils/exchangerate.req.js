42a8db1e2e228eb24308a299b634364a9a4b4220
pan9killer <pan9killer@gmail.com>
2021-11-03 00:29:12 +0400

    axios params fix

--- src/utils/exchangerate.req.js
+++ src/utils/exchangerate.req.js
@@ -2,13 +2,28 @@ const axios = require('axios');
 
 
 export async function Convertor({amount, fromSelected, toSelected}){
-  let convertorData = await axios.get(`https://api.exchangerate.host/convert?base=USD&from=${fromSelected}&to=${toSelected}&places=2&amount=${amount}`)
-  .then((response) => response.data);
-  return convertorData;
+  try{
+    let convertorData = await axios.get(`https://api.exchangerate.host/convert`, {
+      params: {
+        from: fromSelected,
+        to: toSelected,
+        places: 2,
+        amount: amount
+      }
+    })
+    .then((response) => response.data);
+    return convertorData;
+  }catch(err){
+    console.log(err);
+  }
 }
 
 export async function Symbols(){
-  const symbolsData = await axios.get('https://api.exchangerate.host/symbols')
-  .then((response) => response);
-  return symbolsData;
+  try{
+    const symbolsData = await axios.get('https://api.exchangerate.host/symbols')
+    .then((response) => response);
+    return symbolsData;
+  }catch(err){
+    console.log(err);
+  }
 }
