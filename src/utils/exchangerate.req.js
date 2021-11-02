00546774ee3e10a97f16ec81a85bc028000ea36e
pan9killer <pan9killer@gmail.com>
2021-11-03 00:10:01 +0400

    destructurisation fix

--- src/utils/exchangerate.req.js
+++ src/utils/exchangerate.req.js
@@ -1,19 +1,14 @@
 const axios = require('axios');
 
 
-export async function Convertor(props){
-  const {
-    amount,
-    fromSelected,
-    toSelected
-  } = props;
+export async function Convertor({amount, fromSelected, toSelected}){
   let convertorData = await axios.get(`https://api.exchangerate.host/convert?base=USD&from=${fromSelected}&to=${toSelected}&places=2&amount=${amount}`)
   .then((response) => response.data);
-  return await convertorData;
+  return convertorData;
 }
 
 export async function Symbols(){
   const symbolsData = await axios.get('https://api.exchangerate.host/symbols')
   .then((response) => response);
-  return await symbolsData;
+  return symbolsData;
 }
